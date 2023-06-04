474330f273550a095c9868e209488dc6ad753e39
Aayush <amaharjann01@gmail.com>
2023-06-04 00:23:15 -0700

    using the components in App

---
+++ website/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Nav from "../../components/Nav/nav";
+import Inventory from "../../components/Inventory/inventory"
+
+
+export default function App() {
+  return (
+    <div>
+      <Nav />
+      <Inventory />
+    </div>
+  )
+}
